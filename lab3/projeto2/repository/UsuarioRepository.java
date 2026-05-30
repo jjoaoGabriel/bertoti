50a4e37fa83cd3692dd0aafd5d4c960212d96820
Joao Gabriel <joaogabrielf1231@gmail.com>
2026-05-29 21:32:39 -0300

    Delete lab3 directory

--- lab3/projeto2/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.projeto1.repository;
-
-import com.exemplo.projeto1.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
